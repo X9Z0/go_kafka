fd32b0bc780b684f1fa127a199df4ba6034152bb
X9Z0 <x9z0cofd@gmail.com>
2025-01-04 07:01:52 +0530

    producer code simple web app

---
+++ producer/producer.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+type Comment struct {
+	Text string `json:"text" form:"text"`
+}
+
+func main() {
+	app := fiber.New()
+	api := app.Group("/api/v1")
+	api.Post("/comments", createComment)
+	app.Listen(":3000")
+}
+
+func createComment(c *fiber.Ctx) error {
+	cmt := new(Comment)
+	if err := c.BodyParser(cmt); err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+		return err
+
+		cmtInBytes, err := json.Marshal(cmt)
+		ushCommentToQueue("comments", cmtInBytes)
+		if err != nil {
+			log.Println(err)
+		}
+		err = c.JSON(&fiber.Map{
+			"success": true,
+			"message": "comment pushed successfully",
+			"comment": cmt,
+		})
+
+		if err != nil {
+			c.Status(500).JSON(&fiber.Map{
+				"success": false,
+				"message": "Error creating product",
+			})
+			return err
+		}
+	}
+
+}
